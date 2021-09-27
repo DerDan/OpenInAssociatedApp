bd039b0c499fb1d2cde2c77d3aa567cfa35ceb19
GitHub Action <action@github.com>
2021-09-27 19:38:39 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/derdan/openinassociatedapp/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.derdan.openinassociatedapp.services
+
+import com.github.derdan.openinassociatedapp.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
